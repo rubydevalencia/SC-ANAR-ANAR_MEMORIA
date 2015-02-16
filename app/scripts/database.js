02961e3ee7114a6924beae4e81231cf49029e24c
Alberto Cola <betocolsf@gmail.com>
2015-02-16 18:43:46 -0430

    Added highscore functions

--- app/scripts/database.js
+++ app/scripts/database.js
@@ -1,4 +1,10 @@
-var db = new PouchDB('ANAR');
+// Atts: id, password, levels [1..*], cards [1..*], highscore
+var db_user = new PouchDB('ANAR_USER');
+// Atts: id, name, numPieces, time, difficulty
+// Functions: getPieces(), getCardsOnWin(), getPoints()
+var db_level = new PouchDB('ANAR_LEVEL');
+// Atts: id, name, image, description
+var db_card = new PouchDB('ANAR_CARD');
 
 /*
  * User module
@@ -8,24 +14,77 @@ function DBregisterUser(id, password, callback) {
   
   var user = {
     _id: id,
-    password: password
-    // TTODO add rest
+    password: password,
+    highscore: 0,
+    levels: [],
+    cards: [] 
   };
 
-  db.put(user).then(function (result) {
+  db_user.put(user).then(function (result) {
     callback(200);
+  
   }).catch(function (err) {
     callback(err.status);
   });
 }
 
 function DBloginUser(id, password, callback) {
-  db.get(id).then(function (doc) {
+  db_user.get(id).then(function (doc) {
       if (doc.password == password)
         callback(200);
       else
         callback(401);
+  
   }).catch(function (err) {
     callback(err.status);
   });
 }
+
+function DBgetUserHighscore(id) {
+  db_user.get(id).then(function (doc) {
+    return doc.highscore;
+  
+  }).catch(function (err) {
+    consol.log(err);
+  });
+}
+
+function DBsetUserHighscore(id, new_highschore) {
+  db_user.get(id).then(function (doc) {
+    doc.highscore = new_highschore;
+    db_user.put(doc);
+
+  }).catch(function (err) {
+    console.log(err);
+  });
+}
+
+function DBaddScoreToUserHighscore(id, score) {
+  db_user.get(id).then(function (doc) {
+    doc.highscore += score;
+    return db_user.put(doc);
+  
+  }).catch(function (err) {
+    console.log(err);
+  });
+}
+
+/*
+ * End User module
+ */
+
+/*
+ * Level module
+ */
+
+/*
+ * End Level module
+ */
+
+/*
+ * Card module
+ */
+
+/*
+ * End Card module
+ */
