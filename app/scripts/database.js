c303fd87253315de095d6a4119e88113e008a2ca
= <daniel.mallcott@gmail.com>
2015-02-18 17:40:33 -0430

    Changes.

--- app/scripts/database.js
+++ app/scripts/database.js
@@ -2,9 +2,27 @@
 var db_user = new PouchDB('ANAR_USER');
 // Atts: id, name, numPieces, time, difficulty
 // Functions: getPieces(), getCardsOnWin(), getPoints()
-var db_level = new PouchDB('ANAR_LEVEL');
+var db_level = new PouchDB('ANAR_LEVEL2', function(err, result) {
+    if (!err) {
+        console.log('Database \'' + dbName + '\' opened');  // Happens when I first create 'Test'
+    } else {
+        console.log('Database \'' + dbName + '\' failed to open');  // Happens when I try to re-create 'Test'
+    }
+});
 // Atts: id, name, image, description
 var db_card = new PouchDB('ANAR_CARD');
+
+/*PouchDB.destroy('ANAR_LEVEL', function(err, info) { 
+    console.log(err);
+});*/
+
+function destroy() {
+        /*PouchDB.destroy('ANAR_CARD').then(function () {
+          console.log('after destroy()');
+        }).catch(function (err) {
+          console.log('destroy err=' + err);
+        });*/
+      }
 
 /*
  * User module
@@ -16,7 +34,7 @@ function DBregisterUser(id, password, callback) {
     _id: id,
     password: password,
     highscore: 0,
-    levels: [],
+    levels: ["1"],
     cards: [] 
   };
 
@@ -117,6 +135,33 @@ function DBgetUserCardsIds(id, callback) {
 /*
  * Level module
  */
+    // Atts: id, name, numPieces, time, difficulty
+    function DBCreateLevels() {
+        var level = {};
+
+        for (var i = 0; i < 30; i++) {
+            if (i < 10)
+                level = {_id : i.toString(), name : 'Nivel ' + i, numPieces : 10, time : '120', difficulty : 'easy'};
+            else if (i < 20) 
+                level = {_id : i.toString(), name : 'Nivel ' + i, numPieces : 20, time : '60', difficulty : 'medium'};
+            else
+                level = {_id : i.toString(), name : 'Nivel ' + i, numPieces : 30, time : '30', difficulty : 'hard'};
+
+            db_level.put(level);
+        }
+    }
+
+    /* Gets the user levels. */
+    function DBGetLevelsByUser(user_id, callback) {
+        db_user.get(user_id, function (err, user) {
+            if (err)
+                return callback(err, null);
+
+            db.allDocs({keys: user.levels}, function(err, response) {
+                callback(err, response);
+            });
+        });
+    }
 
 function DGgetLevel(id) {
   db_level.get(id).then(function (doc) {
