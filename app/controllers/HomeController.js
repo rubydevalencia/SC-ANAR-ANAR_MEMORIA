87bff1b74b7b875f5c40020b0d12c0d01bedf646
= <daniel.mallcott@gmail.com>
2015-02-21 13:49:06 -0430

    Mostrando los niveles sin fotos.

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -4,34 +4,53 @@ app.controller('HomeController', function ($scope) {
     $scope.levels = {};
     $scope.levelsByCategory = {};
 
+    function pushLevel(levels, level) {
+
+    };
+
     // First we count how many levels per difficulty are
     DBGetLevels(function (err, result) {
         if (err)
             return;
 
-        var easyLevels = 0;
-        var mediumLevels = 0;
-        var hardLevels = 0;
+        var levels = [];
+        var difficulties = [];
 
+        // First we loop through the levels to get the different difficulties
         for (var i = 0; i < result.rows.length; i++) {
-            switch (result.rows[i].doc.difficulty) {
-                case 'easy':
-                    easyLevels++;
-                case 'medium':
-                    mediumLevels++;
-                case 'hard':
-                    hardLevels++;
+            var difficulty = result.rows[i].doc.difficulty;
+
+            if (difficulties.indexOf(difficulty) == -1)
+                difficulties.push(difficulty);
+        }
+
+        // Then we create the objects in the levels array followinf this pattern
+        // {difficulty, [levels]}
+        for (var i = 0; i < difficulties.length; i++) {
+            levels.push({
+                name : difficulties[i], 
+                levels : []
+            });
+        }
+
+        // The we loop again through the levels adding each to the levels inner array
+        for (var i = 0; i < result.rows.length; i++) {
+            var level = result.rows[i].doc;
+
+            for (var j = 0; j < levels.length; j++) {
+                if (levels[j].name == level.difficulty)
+                    levels[j].levels.push(level);
             }
         }
 
-        $scope.levelsByCategory.easyLevels = easyLevels;
-        $scope.levelsByCategory.mediumLevels = mediumLevels;
-        $scope.levelsByCategory.hardLevels = hardLevels;
+        // Then we fill the levels array with actual levels
+
+        $scope.levels = levels;
         $scope.$apply();
     });
 
     // Then we get the user levels
-    DBGetUserLevels($scope.user, function (err, result) {
+    /*DBGetUserLevels($scope.user, function (err, result) {
         var levels = [];
         for (var i = 0; i < result.rows.length; i++) {
             levels.push(result.rows[i].doc);
@@ -39,6 +58,18 @@ app.controller('HomeController', function ($scope) {
 
         $scope.levels = levels;
         $scope.$apply();
-    });
+    });*/
+
+    $scope.getlevels = function(n, levels) {
+        var result = [];
+        for (var i = 0; i < 5; i++) {
+            result.push(levels[n*2 + i]);
+        }
+        return result;
+    };
+
+    $scope.divideRows = function(length) {
+        return new Array(length / 5);
+    };
 });
 
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
 var app = angular.module('AnarApp', ['ui.router']);
+
+app.filter('range', function() {
+  return function(input, total) {
+    total = parseInt(total);
+    for (var i=0; i<total; i++)
+      input.push(i);
+    return input;
+  };
+});
