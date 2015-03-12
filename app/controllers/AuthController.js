ae6e30e5c0a721cced97c61968436a63fe763c10
= <daniel.mallcott@gmail.com>
2015-03-11 21:48:07 -0430

    Listo.

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,11 +1,26 @@
 'use strict';
 
 app.controller('AuthController', function($scope) {
+    $scope.errorMessage = "";
+
     // This function is used to login the user
     $scope.loginUser = function (id, password) {
-        var resultCode = DBloginUser(id, password, function(err, response){
-            if (err)
-               return console.log(err); 
+        if(!id || !password)
+            return;
+
+        DBloginUser(id, password, function(err, response){
+            if (err) {
+                console.log(err); 
+                if (err == 401)
+                    $scope.errorMessage = "Contraseña incorrecta";
+                else if (err == 404)
+                    $scope.errorMessage = "Usuario no encontrado";
+                else
+                    $scope.errorMessage = err;
+                $scope.showErrorMessage();
+                $scope.$apply();
+                return;
+            }
 
             $scope.changeUser(response);
             $scope.changePage('home');  
@@ -14,12 +29,33 @@ app.controller('AuthController', function($scope) {
 
     // This function is used to register the user
     $scope.registerUser = function (id, password) {
+        if(!id || !password)
+            return;
+
         DBregisterUser(id, password, function(err, response){
-            if (err)
-               return console.log(err); 
+            if (err) {
+                console.log(err); 
+                if (err == 409)
+                    $scope.errorMessage = "El usuario ya existe";
+                else if (err == 404)
+                    $scope.errorMessage = "Usuario no encontrado";
+                else
+                    $scope.errorMessage = err;
+                $scope.showErrorMessage();
+                $scope.$apply();    
+                return;
+            }
 
             $scope.changeUser(response);
             $scope.changePage('home');
         });
+    }
+
+    $scope.hideErrorMessage = function () {
+        document.getElementById("error").style.display='none';
+    }
+
+    $scope.showErrorMessage = function () {
+        document.getElementById("error").style.display='block';
     }
 });
