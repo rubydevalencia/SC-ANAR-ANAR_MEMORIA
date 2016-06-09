1e9100ff1c068a484774072c0386b3e59f8ac94d
AriannaAmayaC <arianna.amaya4@gmail.com>
2016-06-08 20:59:18 -0400

    Datos curiosos

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -9,15 +9,48 @@ app.controller('HomeController', function ($scope) {
 
 	//El texto de los datos curiosos se define aquí
 	var datosCuriosos = [
-"En las figuras rupestres, priva la frontalidad\n, lo plano, lo lineal y\
+"En las figuras rupestres, priva la frontalidad,\n lo plano, lo lineal y\
  una\n marcada inclinación hacia lo geométrico.",
 "Una de las características más resaltantes de\n las figuras rupestres en\
  general, \nes la pureza de sus formas.",
-"Los Petroglifos, son figuras realizadas en bajorrelieve\n, grabadas en las\
- rocas o piedras\n, con diferentes técnicas de percusión o abrasión.",
+"Los Petroglifos, son figuras realizadas en bajorrelieve,\ngrabadas en las\
+ rocas o piedras,\n con diferentes técnicas de percusión o abrasión.",
 "Las Pinturas Rupestres, también llamadas Pictografías, son\n dibujos realizados\
  sobre las rocas, mediante la aplicación\n de pigmentos, de origen mineral, vegetal\
- y animal." 
+ y animal.",
+"En la realización de las figuras en los Petroglifos, se utilizaron\ndiferentes\
+ técnicas: Los surcos los trabajaron con percusión\ndirecta e indirecta, y para alisar\
+ y suavizar el surco con conchas\no arena,en una técnica que se llama abrasión. Las\
+ pictografías\no pinturas rupestres, también las hicieron de diferentes maneras:\
+\nUtilizando un palito o huesito hueco para soplar, dibujando\ncon ramas como si fuera\
+ un pincel o usando el dedo.",
+"El color de una pictografía está dado por los pigmentos.\nEl rojo es el color más\
+ utilizado en la mayoría de\nlas pictografías halladas en Venezuela y alrededor del\
+ mundo.",
+"Los Petroglifos, también conocidas como grabados rupestres,\nfueron elaborados\
+ al sustraer material de la\nsuperficie rocosa con instrumentos de una dureza\
+ superior.",
+ "Para lograr los surcos, el antiguo ejecutor pudo utilizar\npunteros de piedra u\
+ otros elementos elaborados\nespecíficamente para tal fin; pero no es frecuente hallar\
+\nherramientas de este tipo que se puedan\nasociar con los Petroglifos.",
+"La mayor parte de las pictografías en el mundo son de\n color rojo, pero también se\
+ han encontrado pintadas\nen negro, naranja, amarillo y blanco, entre otros.\nAprovecharon\
+ las grietas y relieves naturales de la roca\npara, sirviéndose del ocre y el carbón,\
+\ndar volumen a los animales",
+"Grafismos que representan al ser humano, de cuerpo entero,\nla cabeza, con huellas\
+ de manos y pies, como abstracción\n de la figura, es lo que se llama Figuras\
+ Antropomorfas;\nlas representaciones de animales Figuras Zoomorfas;\ne imágenes más\
+ abstractas Figuras Geométricas\no formas libres.",
+"Todas estas figuras nos muestran la realidad exterior e interior,\ndonde y como\
+ transcurría la vida de nuestros\nantepasados, con complejos lenguajes d carácter\
+ universal.",
+"A través de estas figuras, vemos el aporte que nuestros\npueblos han realizado a\
+ la cultura universal, por\neso son un patrimonio de incalculable valor. Deben ser\
+\nprotegidas y tratadas con respeto, para nuestro propio\ndisfrute y el de las\
+ generaciones futuras.",
+"La conservación de este patrimonio cultural\n arqueológico, que no solamente es\
+ venezolano\no americano, sino que pertenece a toda la humanidad,\nes responsabilidad\
+ de todos."
 ]
 
     /*
